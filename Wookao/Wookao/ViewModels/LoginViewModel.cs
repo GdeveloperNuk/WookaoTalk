2b55ca6a44b09ada6ac3c11e0c1b2f76853bda71
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-08-04 11:20:55 +0900

    08/04

---
+++ Wookao/Wookao/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Common.Base;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Messaging;
+//using Models;
+//using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+//using ViewModels.Messaging;
+
+namespace Wookao.ViewModels
+{
+    internal class LoginViewModel
+    {
+    }
+}
